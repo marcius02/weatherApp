a89982fdc0f942177d1436e77e110d11acab0601
marcius02 <marcfernandezz1011@gmail.com>
2025-03-09 19:43:56 +0100

    feat: configure routes for Home, Weather, Map, and About

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,10 +1,20 @@
-import Weather from './Weather'
-
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Home from "./pages/Home";
+import Weather from "./pages/Weather";
+import Map from "./pages/Map";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <>
-      <Weather />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
   );
 }
